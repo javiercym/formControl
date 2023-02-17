933b6285e847c7d89b141d982f4cd2bb899409d1
javier <javiercangahualamarquez@gmail.com>
2023-02-17 09:41:25 -0500

    Creacion de form control Basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formControl';
+  formEnviar!:FormGroup;
+
+  constructor(){
+    
+    this.formEnviar = new FormGroup
+    ({
+      pregunta1: new FormControl(),
+      pregunta2: new FormControl(),
+      pregunta3: new FormControl(),
+      pregunta4: new FormControl(),
+    })
+  }
+
+
+  enviar(){
+    //Los valores del formulario es: 
+    console.log(this.formEnviar.value)
+  }
 }
+
